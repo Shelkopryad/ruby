fd4544e1d5636574ffec1a4479d4e3307a17fb0a
Свирин Денис Александрович <16734975@sigma.sbrf.ru>
2018-08-21 12:06:56 +0300

    fixes

--- examples/sort_methods/sorter.rb
+++ examples/sort_methods/sorter.rb
@@ -1,5 +1,13 @@
 class Sorter
 
+  def create_arr(size)
+    arr = Array.new
+    for i in 0...size
+      arr[i] = rand(1000)
+    end
+    arr
+  end
+
   def selection_sort(arr)
     for i in 0...arr.length
       min, min_i = arr[i], i
@@ -31,12 +39,21 @@ class Sorter
       quick_sort(less) + [pivot] + quick_sort(greater)
     end
   end
-  
+
 end
 
 s = Sorter.new
-a = [8, 5, 3, 7, 2, 0, 4, 9, 1]
+a = s.create_arr(10)
+p a
 p s.selection_sort a
+p
+
+a = s.create_arr(10)
+p a
 p s.bubble_sort a
+p
+
+a = s.create_arr(10)
+p a
 p s.quick_sort a
 # s.q_sort a
