28c02a79cdf432ec44b3633930f94c72045f6600
Свирин Денис Александрович <16734975@sigma.sbrf.ru>
2018-09-14 15:50:12 +0300

    fix

--- rspec/rspec_test.rb
+++ rspec/rspec_test.rb
@@ -7,36 +7,36 @@ end
 
 RSpec.describe Calculator do
   # positive
-  it "should to equal 10" do
+  it "should be equal to 10" do
     expect(Calculator.new.sum(4,6)).to eq 10
   end
   
-  it "should to equal 15" do
+  it "should be equal to 15" do
     expect(Calculator.new.multyply(3, 5)).to eq 15
   end
 
-  it "should to equal 10" do
+  it "should be equal to 10" do
     expect(Calculator.new.substract(15,5)).to eq 10
   end
 
-  it "should to equal 3" do
+  it "should be equal to 3" do
     expect(Calculator.new.divide(18,6)).to eq 3
   end
 
   # negative
-  it "should not to equal 10" do
+  it "shouldn't be equal to 10" do
     expect(Calculator.new.sum(4,7)).to_not eq 10
   end
   
-  it "should not to equal 15" do
+  it "shouldn't be equal to 15" do
     expect(Calculator.new.multyply(3, 9)).to_not eq 15
   end
 
-  it "should not to equal 10" do
+  it "shouldn't be equal to 10" do
     expect(Calculator.new.substract(15,6)).to_not eq 10
   end
 
-  it "should not to equal 3" do
+  it "shouldn't be equal to 3" do
     expect(Calculator.new.divide(18,78)).to_not eq 3
   end
 
--- web_ex/web.rb
+++ web_ex/web.rb
@@ -1,7 +1,5 @@
 require 'socket'
 
-
-
 webserver = TCPServer.new('127.0.0.1', 7125)
 while (session = webserver.accept)
    session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
